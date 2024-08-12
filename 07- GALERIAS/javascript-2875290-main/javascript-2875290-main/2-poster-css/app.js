d57fd284b9918575cd68270e70109ca3652afacc
Pabloandres71 <betancurpablo36@gmail.com>
2024-08-12 14:22:12 -0500

    ultimos trabajos

--- 07- GALERIAS/javascript-2875290-main/javascript-2875290-main/2-poster-css/app.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('IniciarAnimacion').addEventListener('click', function() {
-    const animElements = document.querySelectorAll('.finn, .bmo, .reyhelado, .logo, .dulceprincesa, .jake');
-
-    animElements.forEach(element => {
-        element.style.animationPlayState = 'running';
-    });
-});
